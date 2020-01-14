e55b285c8a47f23bd279488999b58b25d934466a
maxx48lip <44535856+maxx48lip@users.noreply.github.com>
2020-01-14 22:13:21 +0300

    Создал проект

---
+++ HackTravel/HackTravel/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HackTravel
+//
+//  Created by Max Pavlov on 14/01/2020.
+//  Copyright © 2020 Pavlov Max. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		view.backgroundColor = .red
+		// Do any additional setup after loading the view.
+	}
+}
+
