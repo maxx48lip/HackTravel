2b0701c11d714756c27b2a8cab5f096a19b1f27c
Павлов Максим Юрьевич <Pavlov.M.Y@sberbank.ru>
2020-03-20 18:53:12 +0300

    Удалил юзлес код, добавил пример с прогресс вью

---
+++ HackTravel/HackTravel/ColorPallete.swift
@@ -0,0 +1,38 @@
+//
+//  ColorPallete.swift
+//  HackTravel
+//
+//  Created by Max Pavlov on 19/01/2020.
+//  Copyright © 2020 Pavlov Max. All rights reserved.
+//
+
+import UIKit
+
+/// Палитра цветов приложения
+enum ColorPallete {
+
+	/// Черный - (Основной) цвет фона
+	static let trueBlack: UIColor = UIColor(red: 6 / 255, green: 5 / 255, blue: 7 / 255, alpha: 1)
+
+	/// Белый - (Основной) цвет заголовков и кнопок
+	static let trueWhite: UIColor = UIColor(red: 251 / 255, green: 251 / 255, blue: 251 / 255, alpha: 1)
+
+	/// Темный Серый - (Основной) цвет текстов и ячеек
+	static let trueDarkGray: UIColor = UIColor(red: 73 / 255, green: 85 / 255, blue: 77 / 255, alpha: 1)
+
+	/// Светлый Серый - (Альтернативный) цвет текстов и ячеек
+	static let trueLightGray: UIColor = UIColor(red: 165 / 255, green: 165 / 255, blue: 165 / 255, alpha: 1)
+
+	/// Синий - (Основной) цвет для графиков
+	static let trueBlue: UIColor = UIColor(red: 41 / 255, green: 127 / 255, blue: 234 / 255, alpha: 1)
+
+	/// Оранжевый - (Основной) цвет для графиков
+	static let trueOrange: UIColor = UIColor(red: 234 / 255, green: 115 / 255, blue: 41 / 255, alpha: 1)
+
+	/// Зеленый - (Опциональный) цвет для подписей
+	static let trueGreen: UIColor = UIColor(red: 34 / 255, green: 164 / 255, blue: 82 / 255, alpha: 1)
+
+	static let trueRed: UIColor = UIColor(red: 161 / 255, green: 9 / 255, blue: 10 / 255, alpha: 1)
+
+	static let trueDarkRed: UIColor = UIColor(red: 94 / 255, green: 4 / 255, blue: 5 / 255, alpha: 1)
+}
--- HackTravel/HackTravel/ViewController.swift
+++ HackTravel/HackTravel/ViewController.swift
@@ -12,11 +12,20 @@ class ViewController: UIViewController {
 
 	override func viewDidLoad() {
 		super.viewDidLoad()
-		view.backgroundColor = .red
+		view.backgroundColor = .white
 		Debugger.log(type: .info, logString: "Загрузились вьюхи")
 		BaseChain.makeRequest(type: .test, completion: { data in
 			Debugger.log(type: .magic, logString: "\(String(data: data ?? Data(), encoding: .utf8) ?? "nil")")
 		})
 		// Do any additional setup after loading the view.
+		testLoadingView()
+	}
+
+	private func testLoadingView() {
+		let circularView = CircularProgressView()
+		circularView.center = view.center
+		let duration = 1.0
+		circularView.progressAnimation(duration: duration)
+		view.addSubview(circularView)
 	}
 }
